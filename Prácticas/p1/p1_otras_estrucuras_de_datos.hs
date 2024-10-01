aacf83f55a6ab2c35f84b77f1907ea93f5d1dc49
Gonzalo Heredia <gonzalo.heredia.gh8@gmail.com>
2024-10-01 15:56:44 -0300

    Ejercicio 12 arrancado

--- Prácticas/p1/p1_otras_estrucuras_de_datos.hs
+++ Prácticas/p1/p1_otras_estrucuras_de_datos.hs
@@ -43,3 +43,70 @@ evaluar n = foldPoli (+) (*) n id
 
 data AB a = Nil | Bin (AB a) a (AB a)
 
+--I
+
+foldAB :: (b -> a -> b -> b) -> b -> AB a -> b
+foldAB cBin cNil arb = case arb of
+                            Nil -> cNil
+                            Bin i r d -> cBin (rec i) r (rec d)
+                                where rec = foldAB cBin cNil
+
+{-
+Cómo funciona el foldAB
+    En el caso de foldAB, la función que maneja los bin no maneja la estructura interna
+del arbol, solo ve el valor del nodo en el que está parado (representado con un tipo a)
+y tiene permitido seguir la recusión por el árbol de la izquierda y de la derecha (fijate
+que esto tiene tipo b porque la función foldAB devuelve algo de tipo b, entonces el resultado
+de la recurión en los subárboles también devolverá algo de tipo b)
+
+foldAB :: (b -> a -> b -> b)     -- cBin
+          -> b                   -- cNil
+          -> AB a                -- El árbol que le pasas
+          -> b                   -- El resultado de foldAB
+-}
+
+recAB :: (AB a -> a -> AB a -> b -> b -> b) -> b -> AB a -> b
+recAB cBin cNil arb = case arb of
+                            Nil -> cNil
+                            Bin i r d -> cBin i r d (rec i) (rec d)
+                                where rec = recAB cBin cNil
+
+{-
+Cómo funciona el recAB
+    En el caso recAB, la función que maneja los bin sí maneja la estructura interna del árbol,
+entonces la función no estaría recibiendo los resultados de la recursión sobre esos valores
+(al principio) estaría recibiendo los mismos árboles a la izquierda y derecha del valor del nodo
+actual (representado con un tipo a el nodo actual y AB a los árboles de izq y der). Luego, tiene
+permitido seguir la recursión por el árbol de la izquierda y el de la derecha (fijate que esto 
+tiene tipo b porque recAB devuelve algo de tipo b, entonces la recursión en los subárboles también 
+devolverá algo de tipo b)
+
+recAB :: (AB a -> a -> AB a -> b -> b -> b)     cBin
+        -> b                                    cNil
+        -> AB a                                 árbol
+        -> b                                    resultado
+-}
+
+-- II
+
+esNil :: AB a -> Bool
+esNil arb = case arb of
+            Nil -> True
+            otherwise -> False
+
+altura :: AB a -> Int
+altura = foldAB (\ri r rd -> 1 + max ri rd) 0
+
+cantNodos :: AB a -> Int
+cantNodos = foldAB (\ri r rd -> 1 + ri + rd) 0
+
+-- III
+
+mejorSegún :: (a -> a -> Bool) -> AB a -> a
+mejorSegún f = foldAB (\ri r rd -> if (f (cmp (f) ri r) (cmp (f) rd r)) then (cmp f ri r) else (cmp f rd r))
+
+
+cmp :: (a -> a -> Bool) -> AB a -> a -> a
+cmp f arb e = case arb of
+                Nil -> e
+                Bin i r d -> if f r e then r else e
