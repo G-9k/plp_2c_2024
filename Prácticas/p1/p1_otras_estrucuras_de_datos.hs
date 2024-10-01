62e8288395edd53b7186a24ffe0222ebf1d25485
Gonzalo Heredia <gonzalo.heredia.gh8@gmail.com>
2024-10-01 13:57:55 -0300

    Ejercicio 11 terminado

--- Prácticas/p1/p1_otras_estrucuras_de_datos.hs
+++ Prácticas/p1/p1_otras_estrucuras_de_datos.hs
@@ -21,3 +21,25 @@ potencia b exp = foldNat (\x rec -> b*rec) 1 exp
 
 -- Ejercicio 11
 
+data Poli a = X
+            | Cte a
+            | Suma (Poli a) (Poli a)
+            | Prod (Poli a) (Poli a)
+
+foldPoli :: (b -> b -> b) -> (b -> b -> b) -> b -> (a -> b) -> Poli a -> b
+foldPoli cSuma cProd cX cCte poli = case poli of
+                                        X -> cX
+                                        Cte c -> cCte c
+                                        Suma p q -> cSuma (recu p) (recu q)
+                                        Prod p q -> cProd (recu p) (recu q)
+                                        where recu = foldPoli cSuma cProd cX cCte
+
+
+evaluar :: Num a => a -> Poli a -> a
+evaluar n = foldPoli (+) (*) n id
+--n es el numero en el que evaluo
+
+-- Ejercicio 12
+
+data AB a = Nil | Bin (AB a) a (AB a)
+
