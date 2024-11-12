3d2647b3ffdb09ad0c8fbb95493955c1091bd44f
Gonzalo Heredia <gonzalo.heredia.gh8@gmail.com>
2024-11-12 18:52:44 -0300

    ej5.pl terminado

---
+++ Prácticas/p8/ej5.pl
@@ -0,0 +1,25 @@
+% I
+% last(?L, ?U)
+last(L, U) :- append(_, [U], L).
+
+% II
+% reverse(+L, -L1)
+reverse([], []).
+reverse([X|L], L1) :- reverse(L, LR), append(LR, [X], L1).
+
+% III
+% prefijo(?P, +L)
+prefijo(P, L) :- append(P, _, L).
+
+% IV
+% sufijo(?S, +L)
+sufijo(S, L) :- append(_, S, L).
+
+% V
+% sublista(?S, +L)
+sublista(S, L) :- prefijo(P, L), sufijo(S, P).
+
+% VI
+% pertenece(?X, +L)
+pertenece(X, [X|_]).
+pertenece(X, [E|L]) :- X \= E, pertenece(X, L).
