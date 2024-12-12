828cf37fc13abc92d03701ebe5403e764fbc782d
Gonzalo Heredia <gonzalo.heredia.gh8@gmail.com>
2024-12-12 19:11:42 -0300

    simplificación ej caminos

--- Parciales/2C2024/2recu.pl
+++ Parciales/2C2024/2recu.pl
@@ -1,15 +1,11 @@
 desde(X,X).
 desde(X,Y) :- N is X+1, desde(N,Y).
 
-caminoDesde((PX, PY), [(PX,PY)]).
-caminoDesde((PX, PY), [X|XS]) :- X = (PX, PY), desde(1,D), caminoDistancia((PX, PY), D, YS), reverse(YS, XS).
+caminoDesde((PX, PY), XS) :- desde(0,D), caminoDistancia((PX, PY), D, YS), reverse(YS, XS).
 
-caminoDistancia((PX,PY), 1, [(NPX,PY)]) :- NPX is PX + 1.
-caminoDistancia((PX,PY), 1, [(NPX,PY)]) :- NPX is PX - 1.
-caminoDistancia((PX,PY), 1, [(PX,NPY)]) :- NPY is PY + 1.
-caminoDistancia((PX,PY), 1, [(PX,NPY)]) :- NPY is PY - 1.
+caminoDistancia((PX,PY), 0, [(PX,PY)]).
 
-caminoDistancia((PX,PY), D, [(NPX,RPY),(RPX,RPY)|YS]) :- D > 1, D1 is D-1, caminoDistancia((PX,PY), D1, [(RPX,RPY)|YS]), NPX is RPX + 1.
-caminoDistancia((PX,PY), D, [(NPX,RPY),(RPX,RPY)|YS]) :- D > 1, D1 is D-1, caminoDistancia((PX,PY), D1, [(RPX,RPY)|YS]), NPX is RPX - 1.
-caminoDistancia((PX,PY), D, [(RPX,NPY),(RPX,RPY)|YS]) :- D > 1, D1 is D-1, caminoDistancia((PX,PY), D1, [(RPX,RPY)|YS]), NPY is RPY + 1.
-caminoDistancia((PX,PY), D, [(RPX,NPY),(RPX,RPY)|YS]) :- D > 1, D1 is D-1, caminoDistancia((PX,PY), D1, [(RPX,RPY)|YS]), NPY is RPY - 1.
+caminoDistancia((PX,PY), D, [(NPX,RPY),(RPX,RPY)|YS]) :- D > 0, D1 is D-1, caminoDistancia((PX,PY), D1, [(RPX,RPY)|YS]), NPX is RPX + 1.
+caminoDistancia((PX,PY), D, [(NPX,RPY),(RPX,RPY)|YS]) :- D > 0, D1 is D-1, caminoDistancia((PX,PY), D1, [(RPX,RPY)|YS]), NPX is RPX - 1.
+caminoDistancia((PX,PY), D, [(RPX,NPY),(RPX,RPY)|YS]) :- D > 0, D1 is D-1, caminoDistancia((PX,PY), D1, [(RPX,RPY)|YS]), NPY is RPY + 1.
+caminoDistancia((PX,PY), D, [(RPX,NPY),(RPX,RPY)|YS]) :- D > 0, D1 is D-1, caminoDistancia((PX,PY), D1, [(RPX,RPY)|YS]), NPY is RPY - 1.
