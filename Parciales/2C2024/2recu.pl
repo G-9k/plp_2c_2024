ac900dfa52fa20d21b1b14a654029f683f20cd16
Gonzalo Heredia <gonzalo.heredia.gh8@gmail.com>
2024-12-13 18:27:39 -0300

    camino alternativa

--- Parciales/2C2024/2recu.pl
+++ Parciales/2C2024/2recu.pl
@@ -9,3 +9,13 @@ caminoDistancia((PX,PY), D, [(NPX,RPY),(RPX,RPY)|YS]) :- D > 0, D1 is D-1, camin
 caminoDistancia((PX,PY), D, [(NPX,RPY),(RPX,RPY)|YS]) :- D > 0, D1 is D-1, caminoDistancia((PX,PY), D1, [(RPX,RPY)|YS]), NPX is RPX - 1.
 caminoDistancia((PX,PY), D, [(RPX,NPY),(RPX,RPY)|YS]) :- D > 0, D1 is D-1, caminoDistancia((PX,PY), D1, [(RPX,RPY)|YS]), NPY is RPY + 1.
 caminoDistancia((PX,PY), D, [(RPX,NPY),(RPX,RPY)|YS]) :- D > 0, D1 is D-1, caminoDistancia((PX,PY), D1, [(RPX,RPY)|YS]), NPY is RPY - 1.
+
+
+% caminoDesde((PX,PY), [(PX,PY)]).
+% caminoDesde((PX,PY), C) :- caminoDesde((PX,PY), Z), append(_, [Ult], Z), camino(Ult, D), append(Z, [D], C).
+
+% camino((X,Y), (NX,Y)) :- NX is X + 1.
+% camino((X,Y), (NX,Y)) :- NX is X - 1.
+% camino((X,Y), (X,NY)) :- NY is Y + 1.
+% camino((X,Y), (X,NY)) :- NY is Y - 1.
+
